c186bfcfb2b41e751e3cfa98a6f1a73ebf6f2f3f
Richardson, Patrick <prichardson@cj.com>
2015-09-02 10:14:14 -0500

    fixed issue where tabs not displaying correctly

--- js/app.js
+++ js/app.js
@@ -18,16 +18,16 @@
   });
 
 
-  app.controller('PanelController',function(){
-    this.tab = 1;
-    
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    }
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
-    }
-  });
+//  app.controller('PanelController',function(){
+//    this.tab = 1;
+//    
+//    this.selectTab = function(setTab){
+//      this.tab = setTab;
+//    }
+//    this.isSelected = function(checkTab){
+//      return this.tab === checkTab;
+//    }
+//  });
   
   app.controller('ReviewController', function(){
     this.review = {};
